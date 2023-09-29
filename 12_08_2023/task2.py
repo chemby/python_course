a87972e7b958200eed7013af2aca3dfe54ffe3ed
chembyy <86206730+chemby@users.noreply.github.com>
2023-09-29 18:52:52 +0300

    commit

---
+++ 12_08_2023/task2.py
@@ -0,0 +1,12 @@
+CarSpecs = {
+    "gear": "2WD",
+    "engine": "2,5 Hybrid",
+    "length": 4.885,
+    "width": 1.840,
+    "height": 1.445,
+    "color": "black",
+    "maximum_power": 218,
+    "acceleration": 8.3,
+    "weight": 2100,
+    "components": ["wheels", "body", "lights", "wheel", "seats", "engine", "fuel_tank"]
+}
