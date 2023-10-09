88485c22675171baf041bf12c63a876f236f0ca8
chembyy <86206730+chemby@users.noreply.github.com>
2023-10-09 10:29:45 +0300

    commit

--- 12_09_2023.py
+++ 12_09_2023.py
@@ -1,9 +1,8 @@
 def decorate_as_string(func):
 
-    def wrapper(*args):
-        result = func(*args)
+    def wrapper(*args, **kwargs):
+        result = func(*args,**kwargs)
         return str(result)
-
     return wrapper
 
 @decorate_as_string
@@ -11,5 +10,5 @@ def subtract(number1: int, number2: int) -> int:
     return number1 - number2
 
 
-print(subtract(10,24))
-print(type(subtract(10,24)))
+print(subtract(10,number2=24))
+# print(type(subtract(10,24)))
